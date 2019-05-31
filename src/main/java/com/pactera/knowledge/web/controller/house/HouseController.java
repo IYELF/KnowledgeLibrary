41641c8a95f0f54daf8077fbc97b6bd8b576c93c
IYELF <381096381@qq.com>
2019-05-31 16:56:49 +0800

    # WARNING: head commit changed in the meantime
    删除测试controller

--- src/main/java/com/pactera/knowledge/web/controller/house/HouseController.java
+++ src/main/java/com/pactera/knowledge/web/controller/house/HouseController.java
@@ -1,12 +1,25 @@
 package com.pactera.knowledge.web.controller.house;
 
-import org.springframework.stereotype.Controller;
+import java.util.List;
 
-@Controller
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.pactera.knowledge.base.ApiResponse;
+import com.pactera.knowledge.entity.dto.SubwayDTO;
+import com.pactera.knowledge.entity.dto.SubwayStationDTO;
+import com.pactera.knowledge.entity.dto.SupportAddressDTO;
+import com.pactera.knowledge.service.ServiceMultiResult;
+import com.pactera.knowledge.service.hourse.IAddressService;
+
+@RestController
 public class HouseController {
 
-//    @Autowired
-//    private IAddressService addressService;
+    @Autowired
+    private IAddressService addressService;
 //
 //    @Autowired
 //    private IHouseService houseService;
@@ -31,66 +44,66 @@ public class HouseController {
 //        return ApiResponse.ofSuccess(result.getResult());
 //    }
 //
-//    /**
-//     * 获取支持城市列表
-//     * @return
-//     */
-//    @GetMapping("address/support/cities")
-//    @ResponseBody
-//    public ApiResponse getSupportCities() {
-//        ServiceMultiResult<SupportAddressDTO> result = addressService.findAllCities();
-//        if (result.getResultSize() == 0) {
-//            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
-//        }
-//        return ApiResponse.ofSuccess(result.getResult());
-//    }
-//
-//    /**
-//     * 获取对应城市支持区域列表
-//     * @param cityEnName
-//     * @return
-//     */
-//    @GetMapping("address/support/regions")
-//    @ResponseBody
-//    public ApiResponse getSupportRegions(@RequestParam(name = "city_name") String cityEnName) {
-//        ServiceMultiResult<SupportAddressDTO> addressResult = addressService.findAllRegionsByCityName(cityEnName);
-//        if (addressResult.getResult() == null || addressResult.getTotal() < 1) {
-//            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
-//        }
-//        return ApiResponse.ofSuccess(addressResult.getResult());
-//    }
-//
-//    /**
-//     * 获取具体城市所支持的地铁线路
-//     * @param cityEnName
-//     * @return
-//     */
-//    @GetMapping("address/support/subway/line")
-//    @ResponseBody
-//    public ApiResponse getSupportSubwayLine(@RequestParam(name = "city_name") String cityEnName) {
-//        List<SubwayDTO> subways = addressService.findAllSubwayByCity(cityEnName);
-//        if (subways.isEmpty()) {
-//            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
-//        }
-//
-//        return ApiResponse.ofSuccess(subways);
-//    }
-//
-//    /**
-//     * 获取对应地铁线路所支持的地铁站点
-//     * @param subwayId
-//     * @return
-//     */
-//    @GetMapping("address/support/subway/station")
-//    @ResponseBody
-//    public ApiResponse getSupportSubwayStation(@RequestParam(name = "subway_id") Long subwayId) {
-//        List<SubwayStationDTO> stationDTOS = addressService.findAllStationBySubway(subwayId);
-//        if (stationDTOS.isEmpty()) {
-//            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
-//        }
-//
-//        return ApiResponse.ofSuccess(stationDTOS);
-//    }
+    /**
+     * 获取支持城市列表
+     * @return
+     */
+    @GetMapping("address/support/cities")
+    @ResponseBody
+    public ApiResponse getSupportCities() {
+        ServiceMultiResult<SupportAddressDTO> result = addressService.findAllCities();
+        if (result.getResultSize() == 0) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
+        }
+        return ApiResponse.ofSuccess(result.getResult());
+    }
+
+    /**
+     * 获取对应城市支持区域列表
+     * @param cityEnName
+     * @return
+     */
+    @GetMapping("address/support/regions")
+    @ResponseBody
+    public ApiResponse getSupportRegions(@RequestParam(name = "city_name") String cityEnName) {
+        ServiceMultiResult<SupportAddressDTO> addressResult = addressService.findAllRegionsByCityName(cityEnName);
+        if (addressResult.getResult() == null || addressResult.getTotal() < 1) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
+        }
+        return ApiResponse.ofSuccess(addressResult.getResult());
+    }
+
+    /**
+     * 获取具体城市所支持的地铁线路
+     * @param cityEnName
+     * @return
+     */
+    @GetMapping("address/support/subway/line")
+    @ResponseBody
+    public ApiResponse getSupportSubwayLine(@RequestParam(name = "city_name") String cityEnName) {
+        List<SubwayDTO> subways = addressService.findAllSubwayByCity(cityEnName);
+        if (subways.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
+        }
+
+        return ApiResponse.ofSuccess(subways);
+    }
+
+    /**
+     * 获取对应地铁线路所支持的地铁站点
+     * @param subwayId
+     * @return
+     */
+    @GetMapping("address/support/subway/station")
+    @ResponseBody
+    public ApiResponse getSupportSubwayStation(@RequestParam(name = "subway_id") Long subwayId) {
+        List<SubwayStationDTO> stationDTOS = addressService.findAllStationBySubway(subwayId);
+        if (stationDTOS.isEmpty()) {
+            return ApiResponse.ofStatus(ApiResponse.Status.NOT_FOUND);
+        }
+
+        return ApiResponse.ofSuccess(stationDTOS);
+    }
 //
 //    @GetMapping("rent/house")
 //    public String rentHousePage(@ModelAttribute RentSearch rentSearch,
